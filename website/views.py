8743224738b7497910145697693138f1935098d6
Braydon Pettit <braydonpettit24@gmail.com>
2024-07-20 22:50:37 +1000

    styling and database tables

--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
     # Login Logic
     if request.method == "POST":
         username = request.POST['username']
@@ -19,7 +21,7 @@ def home(request):
                 request, ("Error logging in, please try again..."))
             return redirect('home')
     else:
-        return render(request, "home.html", {})
+        return render(request, "home.html", {'records':records})
 
 
 def logout_user(request):
@@ -28,4 +30,18 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, "register.html", {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and Login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have registered successfully!"))
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, "register.html", {'form':form})
+    return render(request, "register.html", {'form':form})
